eef385bf5c845f45a142e0addc2a1d09246ceb45
pekarsky.maxim@gmail.com <flufci@gmail.com>
2016-08-07 19:57:07 -0400

    update menu, toggle, and content

--- script.js
+++ script.js
@@ -6,29 +6,41 @@ $(document).ready(main);
 
 function main(){
 
+    var menu = false;
+
     console.log("ready");
 
     $("#toggle").click(function(){
-        console.log("open menu"); 
-        $("#menu").show();
-        $("#landing").css("opacity", "1");
-        $("#landing").addClass("blur");
-    //    $("#divider").show();
+
+        if(menu){
+            menu = false;
+            console.log("close menu"); 
+            $("#menu").hide();
+            $("#landing").css("opacity", "1");
+            $("#landing").removeClass("blur");
+        } else {
+            menu = true;
+            console.log("open menu"); 
+            $("#menu").show();
+            $("#landing").css("opacity", "1");
+            $("#landing").addClass("blur");
+        }
+
+
+        
 
     });
 
-    $("#close").click(function(){
-        console.log("close menu"); 
-        $("#menu").hide();
-        $("#landing").css("opacity", "1");
-        $("#landing").removeClass("blur");
-    //    $("#divider").hide();
+    var containerPadding = $("#content_container").css("padding");
 
-    });
+    if ($(window).width() < 960) {
+            console.log("we got small!");
+            $('.photo').hide();
+        }
 
     $(".menu-section").hover(function(){
-        $(this).css("background", "rgba(0, 0, 0, 0.4)");
-        $(this).css("color", "orange");
+        $(this).css("background", "rgba(0, 0, 0, 0.8)");
+        $(this).css("color", "#808FFF");
     }, function(){
         $(this).css("background", "rgba(0, 0, 0, 0.6)");
         $(this).css("color", "white");
